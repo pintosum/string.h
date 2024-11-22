106607b733cab4e4767160df862e5674914deac0
Pinto Sumeragi <pintosum@ev-q1.kzn.21-school.ru>
2024-11-22 19:16:15 +0300

    second

--- string.c
+++ string.c
@@ -1,6 +1,6 @@
 #include "string.h"
 void *memchr(const void *s, int c, size_t n) {
-  unsigned char *ptr = (unsigned char *)s;
+  const unsigned char *ptr = s;
   while (n--)
     if (*ptr++ == (unsigned char)c)
       return (void *)(ptr - 1);
@@ -8,10 +8,26 @@ void *memchr(const void *s, int c, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  unsigned char *p1 = (unsigned char *)s1;
-  unsigned char *p2 = (unsigned char *)s2;
+  const unsigned char *p1 = s1;
+  const unsigned char *p2 = s2;
   while (n--)
     if (*p1++ != *p2++)
       return *(p1 - 1) - *(p2 - 1);
   return 0;
+}
+
+void *memcpy(void *dest, const void *src, size_t n){
+  const unsigned char *s = src;
+  unsigned char *d = dest;
+  while(n--)
+    *d++ = *s++;
+  return (void *)d;
+}
+
+void *memmove(void *dest, const void *src, size_t n){
+  void *cp = malloc(n);
+  memcpy(cp, src, n);
+  memcpy(dest, cp, n);
+  free(cp);
+  return dest;
 }
