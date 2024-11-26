3db8a3fc2c9886a60e523ed82c291b27699dd650
pintosum <oldogg@yandex.ru>
2024-11-26 15:03:46 +0300

    first 15

--- string.c
+++ string.c
@@ -1,5 +1,5 @@
 #include "string.h"
-void *memchr(const void *s, int c, size_t n) {
+void *s21_memchr(const void *s, int c, size_t n) {
   const unsigned char *ptr = s;
   while (n--)
     if (*ptr++ == (unsigned char)c)
@@ -7,7 +7,7 @@ void *memchr(const void *s, int c, size_t n) {
   return NULL;
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
+int s21_memcmp(const void *s1, const void *s2, size_t n) {
   const unsigned char *p1 = s1;
   const unsigned char *p2 = s2;
   while (n--)
@@ -16,7 +16,7 @@ int memcmp(const void *s1, const void *s2, size_t n) {
   return 0;
 }
 
-void *memcpy(void *dest, const void *src, size_t n) {
+void *s21_memcpy(void *dest, const void *src, size_t n) {
   const unsigned char *s = src;
   unsigned char *d = dest;
   while (n--)
@@ -24,64 +24,130 @@ void *memcpy(void *dest, const void *src, size_t n) {
   return (void *)d;
 }
 
-void *memmove(void *dest, const void *src, size_t n) {
-  void *cp = malloc(n);
-  memcpy(cp, src, n);
-  memcpy(dest, cp, n);
-  free(cp);
-  return dest;
-}
-
-void *memset(void *str, int c, size_t n) {
+void *s21_memset(void *str, int c, size_t n) {
   char *p = (char *)str;
   while (n--)
     *p++ = c;
   return (void *)p;
 }
 
-char *strcat(char *dest, const char *src){
+char *s21_strncat(char *dest, const char *src, size_t n) {
   char *ret = dest;
-  while(*dest)
+  while (*dest)
     dest++;
-  while(*src)
+  while (n--)
     *dest++ = *src++;
   return ret;
 }
 
-char *strncat(char *dest, const char *src, size_t n){
+char *s21_strchr(const char *str, int c) {
+  size_t len = s21_strlen(str);
+  while (len--)
+    if (*str++ == c)
+      return (char *)str;
+  return NULL;
+}
+
+int s21_strncmp(const char *str1, const char *str2, size_t n) {
+  int ret = 0;
+  while (n-- && *str1 && *str2 && *str1 == *str2) {
+    str1++;
+    str2++;
+  }
+  if (*str1 < *str2)
+    ret = -1;
+  else if (*str1 > *str2)
+    ret = 1;
+  return ret;
+}
+
+char *s21_strncpy(char *dest, const char *src, size_t n) {
   char *ret = dest;
-  while(*dest)
-    dest++;
-  while(n--)
+  while (n--)
     *dest++ = *src++;
   return ret;
 }
 
-char *strchr(const char *str, int c){
-  while(*str != 0 && *str != c)
-    str++;
-  return (char *)str;
+size_t s21_strcspn(const char *str1, const char *str2) {
+  size_t len1 = s21_strlen(str1);
+  size_t len2 = s21_strlen(str2);
+  for (size_t ret = 0; ret < len1; ret++)
+    for (size_t i = 0; i < len2; i++)
+      if (str2[i] == str1[ret])
+        return ret + 1;
+  return len1;
 }
 
-int strcmp(const char *str1, const char *str2){
-  int ret = 0;
-  while(*str1 && *str2 && *str1 == *str2){
-    str1++;
-    str2++;
-  }
-  if (*str1 < *str2) ret = -1;
-  else if(*str1 > *str2) ret = 1;
+char *s21_strerror(int errnum);
+
+size_t s21_strlen(const char *str) {
+  size_t ret = 0;
+  while (*str++)
+    ret++;
   return ret;
 }
 
-int strncmp(const char *str1, const char *str2, size_t n){
-  int ret = 0;
-  while(n-- && *str1 && *str2 && *str1 == *str2){
-    str1++;
-    str2++;
-  }
-  if (*str1 < *str2) ret = -1;
-  else if(*str1 > *str2) ret = 1;
-  return ret;
+char *s21_strpbrk(const char *str1, const char *str2) {
+  return (char *)(str1 + s21_strcspn(str1, str2));
+}
 
+char *s21_strrchr(const char *str, int c) {
+  size_t len = s21_strlen(str);
+  while (len--)
+    if (str[len] == c)
+      return (char *)str + len;
+  return NULL;
+}
+
+char *s21_strstr(const char *haystack, const char *needle) {
+  if (needle) {
+    int len1 = s21_strlen(haystack);
+    int len2 = s21_strlen(needle);
+    for (int i = 0; i < len1; i++) {
+      if (haystack[i] == needle[0]) {
+        int j = 0;
+        for (; i + j < len1 && j < len2; j++) {
+          if (haystack[i + j] != needle[j])
+            break;
+        }
+        if (j == len2)
+          return (char *)haystack + i;
+      }
+    }
+  }
+  return (char *)haystack;
+}
+
+char *s21_strtok(char *str, const char *delim) {
+  static char *prev;
+  char *ret;
+  if (!str)
+    str = prev;
+  str += s21_strspn(str, delim);
+  if (*str == 0) {
+    prev = str;
+    ret = NULL;
+  } else {
+    ret = str;
+    str += s21_strcspn(str, delim);
+    if (*str == 0)
+      prev = str;
+    else {
+      *str = 0;
+      prev = str + 1;
+    }
+  }
+  return ret;
+}
+
+size_t s21_strspn(const char *str1, const char *str2) {
+  size_t len1 = s21_strlen(str1);
+  size_t len2 = s21_strlen(str2);
+  for (int ret = 0; ret < len1; ret++) {
+    for (int i = 0; i < len2; i++) {
+      if (str1[ret] != str2[i])
+        return ret + 1;
+    }
+  }
+  return len1;
 }
