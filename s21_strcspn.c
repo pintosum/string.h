79d00fb59eecfc534ea33ca5e5fdf8ea92dab13f
Pinto Sumeragi <pintosum@pr-j4.kzn.21-school.ru>
2024-12-01 13:39:58 +0300

    fixed strspn

--- s21_strcspn.c
+++ s21_strcspn.c
@@ -3,9 +3,14 @@
 size_t s21_strcspn(const char *str1, const char *str2) {
   size_t len1 = s21_strlen(str1);
   size_t len2 = s21_strlen(str2);
-  for (size_t i = 0; i < len1; i++)
-    for (size_t j = 0; j < len2 && i != len1; j++)
+  for (size_t i = 0; i < len1; i++) {
+    int matched = 0;
+    for (size_t j = 0; j < len2 && !matched; j++) {
       if (str2[j] == str1[i])
-        len1 = i;
+        matched++;
+    }
+    if (!matched)
+      len1 = i;
+  }
   return len1;
 }
--- s21_strspn.c
+++ s21_strspn.c
@@ -3,9 +3,14 @@
 size_t s21_strspn(const char *str1, const char *str2) {
   size_t len1 = s21_strlen(str1);
   size_t len2 = s21_strlen(str2);
-  for (size_t i = 0; i < len1; i++)
-    for (size_t j = 0; j < len2 && i != len1; j++)
-      if (str1[i] != str2[j])
-        len1 = i;
+  for (size_t i = 0; i < len1; i++) {
+    int matched = 0;
+    for (size_t j = 0; j < len2 && !matched; j++) {
+      if (str1[i] == str2[j])
+        matched++;
+    }
+    if (!matched)
+      len1 = i;
+  }
   return len1;
 }
